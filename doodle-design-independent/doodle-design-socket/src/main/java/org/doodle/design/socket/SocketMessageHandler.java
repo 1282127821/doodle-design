d5632e85f167f57380370bb0340311517e22c2c7
石头 <tingyan.shen@gmail.com>
2023-08-12 16:22:58 +0800

    基于 @rsocket 网络驱动扩展游戏网络驱动

---
+++ doodle-design-independent/doodle-design-socket/src/main/java/org/doodle/design/socket/SocketMessageHandler.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022-present Doodle. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.doodle.design.socket;
+
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.doodle.design.messaging.packet.reactive.PacketMappingMessageHandler;
+import org.springframework.core.codec.StringDecoder;
+
+@Slf4j
+public class SocketMessageHandler extends PacketMappingMessageHandler {
+
+  public SocketServerAcceptor serverAcceptor() {
+    return socketConnection ->
+        socketConnection
+            .onClose()
+            .doFirst(
+                () -> {
+                  log.info(
+                      "连接建立: ID: {} IP: {}",
+                      socketConnection.connection().channel().id().asShortText(),
+                      socketConnection.remoteAddress());
+
+                  socketConnection.connection().onReadIdle(5000, socketConnection::dispose);
+                })
+            .doOnTerminate(
+                () ->
+                    log.info(
+                        "连接关闭: ID: {}",
+                        socketConnection.connection().channel().id().asShortText()));
+  }
+
+  @Override
+  public void afterPropertiesSet() {
+    setDecoders(List.of(StringDecoder.allMimeTypes()));
+    super.afterPropertiesSet();
+  }
+}
