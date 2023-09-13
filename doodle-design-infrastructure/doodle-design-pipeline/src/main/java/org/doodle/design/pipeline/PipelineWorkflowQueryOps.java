096ee6e7ca1083c7902c53e0744bdb25855859be
石头 <tingyan.shen@gmail.com>
2023-09-13 17:34:19 +0800

    CI Pipeline 组件添加 Pipeline/Workflow 接口和数据结构定义

---
+++ doodle-design-infrastructure/doodle-design-pipeline/src/main/java/org/doodle/design/pipeline/PipelineWorkflowQueryOps.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022-present Doodle. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.doodle.design.pipeline;
+
+import org.doodle.design.common.Result;
+import reactor.core.publisher.Mono;
+
+public interface PipelineWorkflowQueryOps {
+
+  @FunctionalInterface
+  interface RSocket {
+    String QUERY_MAPPING = "pipeline.workflow.query";
+
+    Mono<PipelineWorkflowQueryReply> query(PipelineWorkflowQueryRequest request);
+  }
+
+  @FunctionalInterface
+  interface Servlet {
+    String QUERY_MAPPING = "/pipeline/workflow/query";
+
+    Result<org.doodle.design.pipeline.model.payload.reply.PipelineWorkflowQueryReply> query(
+        org.doodle.design.pipeline.model.payload.request.PipelineWorkflowQueryRequest request);
+  }
+}
