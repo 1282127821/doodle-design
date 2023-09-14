59324adae66283cce75bef1cf4d143ec6564a956
石头 <tingyan.shen@gmail.com>
2023-09-14 19:50:01 +0800

    礼包组件 基础 api 和数据结构定义

---
+++ doodle-design-component/doodle-design-giftpack/src/main/java/org/doodle/design/giftpack/GiftPackHashPageOps.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022-present Doodle. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.doodle.design.giftpack;
+
+import org.doodle.design.common.Result;
+import reactor.core.publisher.Mono;
+
+public interface GiftPackHashPageOps {
+
+  @FunctionalInterface
+  interface RSocket {
+    String PAGE_MAPPING = "giftpack.hash.page";
+
+    Mono<GiftPackHashPageReply> page(GiftPackHashPageRequest request);
+  }
+
+  @FunctionalInterface
+  interface Servlet {
+    String PAGE_MAPPING = "/giftpack/hash/page";
+
+    Result<org.doodle.design.giftpack.model.payload.reply.GiftPackHashPageReply> page(
+        org.doodle.design.giftpack.model.payload.request.GiftPackHashPageRequest request);
+  }
+}
