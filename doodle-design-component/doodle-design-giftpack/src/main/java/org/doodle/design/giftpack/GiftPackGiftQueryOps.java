a47f920f0052b19aa51a6a1e210a03340cc99f49
tingyanshen <tingyan.shen@gmail.com>
2023-09-20 03:06:24 +0800

    礼包组件 api 和数据结构设计实现

---
+++ doodle-design-component/doodle-design-giftpack/src/main/java/org/doodle/design/giftpack/GiftPackGiftQueryOps.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022-present Doodle. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.doodle.design.giftpack;
+
+import org.doodle.design.common.Result;
+import reactor.core.publisher.Mono;
+
+public interface GiftPackGiftQueryOps {
+
+  @FunctionalInterface
+  interface RSocket {
+    String QUERY_MAPPING = "giftpack.gift.query";
+
+    Mono<GiftPackGiftQueryReply> query(GiftPackGiftQueryRequest request);
+  }
+
+  @FunctionalInterface
+  interface Servlet {
+    String QUERY_MAPPING = "/giftpack/gift/query";
+
+    Result<org.doodle.design.giftpack.model.payload.reply.GiftPackGiftQueryReply> query(
+        org.doodle.design.giftpack.model.payload.request.GiftPackGiftQueryRequest request);
+  }
+}
